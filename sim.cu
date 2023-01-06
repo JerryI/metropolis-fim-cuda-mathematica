5234d10ca9ac91836443b4f406bd107c6e617cb4
Kirill <krikus.ms@gmail.com>
2023-01-06 19:22:23 +0100

    test

--- sim.cu
+++ sim.cu
@@ -1,7 +1,7 @@
 #include <fstream>
 #include <iostream>
-#include <blaze/Blaze.h>
-#include <blaze/Forward.h>
+//#include <blaze/Blaze.h>
+//#include <blaze/Forward.h>
 
 #include <chrono>
 #include <random>
